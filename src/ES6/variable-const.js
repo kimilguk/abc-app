424e0dde212bf9d8ab20b9cc9107f93bb41beffb
kimilguk <boramcom@daum.net>
2022-12-09 14:27:12 +0900

    리액트02_2교시 작업소스

--- src/ES6/variable-const.js
+++ src/ES6/variable-const.js
@@ -6,7 +6,7 @@ const varialbe3 = [];
 //varialbe3 = ['배열변수', 1, 2, 3]; //Type 에러발생
 varialbe3.push('배열변수');
 varialbe3.splice(1, 0, 1); //(시작인덱스, 삭제할 개수, 추가할 값)
-//varialbe3.pop(); //pop함수로 인덱스의 선입선출방식으로 데이터를 삭제한다.
+//varialbe3.pop(); //pop함수로 Top 인덱스의 데이터를 삭제한다.
 const arr = varialbe3.concat('배열변수',1); //배열에 값을 추가해 신규 배열 생성
 const arr2 = arr.slice(0,1); //배열추출(시작인덱스,끝-1인텍스)및 나머지삭제
 const varialbe4 = {};
