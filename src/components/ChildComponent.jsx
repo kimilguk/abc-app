fb1e5440b59a462063ffe4644762e2e0a774b4be
kimilguk <boramcom@daum.net>
2022-12-21 13:42:14 +0900

    리액트05-1교시 작업소스

--- src/components/ChildComponent.jsx
+++ src/components/ChildComponent.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 //클래스 명은 반드시 대문자로...
-class TodaysPlan extends Component {
+class ChildComponent extends Component {
     render() {
         return (
             <div>
-                기준 컴포넌트 추가
+                작업 컴포넌트 추가
             </div>
         );
     }
 }
 
-export default TodaysPlan;
+export default ChildComponent;
---
+++ src/components/ChildComponentFunc.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ChildComponent(props) {
+    return (
+        <div>
+            작업 함수형 컴포넌트 추가
+        </div>
+    );
+}
+
+export default ChildComponent;
