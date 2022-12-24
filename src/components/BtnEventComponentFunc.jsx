303044738dbdb65cdb90f56b58c72ef366fe4b91
kimilguk <boramcom@daum.net>
2022-12-24 16:56:23 +0900

    리액트06-2교시 작업소스

--- src/components/BtnEventComponentFunc.jsx
+++ src/components/BtnEventComponentFunc.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
 function BtnEventComponentFunc(props) {
-    //state예약어대신 useState함수로 변수초기화(아래)-수정
+    /*state 상태객체 대신 리액트에 내장된 useState 함수를 사용하고,
+    componentWillMount() 생명주기 함수 대신에 useEffect() 함수를 사용한다.*/
     const [state,setState] = useState({count:props.count});
     const clickEvent = () => {
         //state 변경 let count = this.state.count;
@@ -12,7 +13,7 @@ function BtnEventComponentFunc(props) {
     useEffect(()=>{
         console.log('순서:useEffect', props.count);
         setState({count: props.count});//수정
-    },[props.count]);//componentWillMount() == ,[] 두번째 인자는 기능이 같다. 즉, 1번만 실행
+    },[props.count]);//전송 받는 props.count 속성 값이 변경 될 때 setState함수로 변경된 값이 화면이 재생 된다.
     return (
         <div>
             <p>카운트: {state.count}</p>
