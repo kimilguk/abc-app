d8362646538af73cf2484b8d2ac853efe22d337a
kimilguk <boramcom@daum.net>
2022-12-31 19:19:17 +0900

    리액트03-3교시 작업소스

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Dashboard from './dashboard/Dashboard';
+//import Dashboard from './dashboard/Dashboard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Dashboard />
-    {/*<App />*/}
+    {/*<Dashboard />*/}
+    <App />
   </React.StrictMode>
 );
 
