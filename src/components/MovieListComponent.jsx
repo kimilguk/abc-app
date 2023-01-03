931bd37e613580fad16046b35efeff4ab9826b0a
kimilguk <boramcom@daum.net>
2023-01-03 18:30:46 +0900

    리액트10-2교시 작업소스

--- src/components/MovieListComponent.jsx
+++ src/components/MovieListComponent.jsx
@@ -4,22 +4,48 @@ import PropTypes from 'prop-types';
 class MovieListComponent extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            movieList: [
+            { rank: '1', movieNm: '아바타: 물의 길', audiCnt: '100,000', audiAcc: '5,000,000', openDt: '2022-12-25'},
+            { rank: '2', movieNm: '아바타: 물의 길', audiCnt: '100,000', audiAcc: '5,000,000', openDt: '2022-12-25'},
+            ],
+        };
     }
-
+    
     componentWillMount() {
         
     }
 
     componentDidMount() {
-        
+        /* Rest API 통신 기본서식, Promise 객체를 반환
+        fetch(url, options)
+            .then((response) => console.log("response:", response))
+            .catch((error) => console.log("error:", error));
+            Promise 객체는 비동기 작업의 단위 이자 비동기 처리에 사용되는 객체.
+                API호출이 성공했을 경우(then)에는 응답(response) 객체를 resolve(성공-답)하고
+                실패했을 경우(catch)에는 예외(error) 객체를 reject(실패-거부)합니다.
+        */
+        const URL = 'http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20221225';
+        fetch(URL)
+            .then((response) => {
+                    //console.log(response.json());
+                    return response.json();//프로미스 객체로 리턴 반환 된다.(아래 then 으로)
+                }
+            ).then(data => { //도트 연산자로 함수를 이어서 편리하게 사용하는 방식을 체인 함수라고 한다.
+                console.log(data['boxOfficeResult']['dailyBoxOfficeList']);//크롬 브라우저에서 json데이터 확인
+                this.setState(prevState => ({ //json 데이터는 키 값으로 데이터를 분리 할 수 있다.
+                    movieList: data['boxOfficeResult']['dailyBoxOfficeList'],
+                }));
+                return data;
+            });
     }
 
     componentWillReceiveProps(nextProps) {
 
     }
-
+    //아래 shouldComponentUpdate()함수는 setState로 state 상태 값이 업데이트되면, 화면을 새로 고침 한다.
     shouldComponentUpdate(nextProps, nextState) {
-        //return true;
+        return true; //true 가 없으면 화면이 업데이트 되지 않는다.
     }
 
     componentWillUpdate(nextProps, nextState) {
@@ -80,13 +106,22 @@ class MovieListComponent extends Component {
                     </tr>
                     </thead>
                     <tbody>
-                    <tr>
+                    {/* <tr>
                         <th scope="row">1</th>
                         <td><a href="movie-read.html">아바타: 물의 길</a></td>
                         <td>100,000</td>
                         <td>5,000,000</td>
                         <td>2022-12-25</td>
-                    </tr>
+                    </tr> */}
+                    {this.state.movieList.map((movie) => 
+                        <tr key={movie.rank}>
+                            <th scope="row">{movie.rank}</th>
+                            <td><a href="movie-read.html">{movie.movieNm}</a></td>
+                            <td>{movie.audiCnt}</td>
+                            <td>{movie.audiAcc}</td>
+                            <td>{movie.openDt}</td>
+                        </tr>)
+                    }
                     </tbody>
                 </table>
                 <nav aria-label="Page navigation example">
