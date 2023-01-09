eaf129d574c54243f23f9e4cef9df9485bf5a54e
kimilguk <boramcom@daum.net>
2023-01-09 17:31:24 +0900

    리액트13-1교시 작업소스

--- src/components/NaverLogin.js
+++ src/components/NaverLogin.js
@@ -1,15 +1,15 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-const NaverLogin = () => {
+function NaverLogin(props) {
     //화면 갱신용 state 변수 추가(아래)
     const [userInfo,setUserInfo] = useState(
         {
-            user_name: (localStorage.getItem('user_name'))?localStorage.getItem('user_name'):'',
+            user_name: '',
             user_email: '',
             profile_image: '',
         }
     );
-    const { naver } = window
+    const { naver } = window;
     const NAVER_CLIENT_ID = 'YXJYaCx0fJnawGGVopsc'; 
     const NAVER_CALLBACK_URL = "http://127.0.0.1:3000";
     const initializeNaverLogin = () => {
@@ -17,13 +17,13 @@ const NaverLogin = () => {
             clientId: NAVER_CLIENT_ID,
             callbackUrl: NAVER_CALLBACK_URL,
             isPopup: false,// 팝업창으로 로그인을 진행할지 여부
-            loginButton: { color: 'green', type: 3, height: 58 },// 버튼 타입 ( 색상, 타입, 크기 변경 가능 )
-            callbackHandle: true,
+            loginButton: { color: 'green', type: 3, height: 58 },//네이버 전용 버튼 타입 ( 색상, 타입, 크기 변경 가능 )
+            callbackHandle: true, //버튼 모양 변경을 핸들러 함수에서 처리 할 수 있다.
         })
-        naverLogin.init()
-        // 내장된 naverLogin 을 이용하여 유저(사용자) 정보를 불러오는데 아래와 같이 naverLogin.user 처런 직접 접근하여 추출가능하다.
+        naverLogin.init();
+        // naverLogin 을 이용하여 사용자정보를 불러오는데 아래와 같이 naverLogin.user 처럼 직접 접근하여 추출 가능하다.
         // 이때, 데이터는 네이버에서 동의한 데이터만 추출 가능하다.
-        // async가 붙은 함수는 {내부 작업이 마치기 전 다른 작업을 못하고, 동기 작업처럼 진행 된다.} 
+        // async ~ [await는 여기선 안 보인다.]가 붙은 함수는 {내부 작업}이 마친 후 다른 작업이 진행 되는 동기 작업처럼 된다.
         naverLogin.getLoginStatus(async function (status) {
             if (status) {
                 console.log("네이버에서 받은 status :", status);
@@ -33,12 +33,7 @@ const NaverLogin = () => {
                     user_email: naverLogin.user.getEmail(),
                     profile_image: naverLogin.user.profile_image,
                 });
-                localStorage.clear();
-                localStorage.setItem('user_name', naverLogin.user.getName())
-                localStorage.setItem('user_email', naverLogin.user.getEmail())
-                localStorage.setItem('profile_image', naverLogin.user.profile_image)
                 console.log("사용자 정보", naverLogin.user);
-                console.log("로컬 스토리지 :", localStorage.getItem('user_name'));
             }
         })
     }
@@ -47,7 +42,6 @@ const NaverLogin = () => {
     },[]); //생명주기 함수대신 useEffectr()함수사용,[] 의미는 1번만 실행
     //로그아웃 시 로그인 스토리지 정보 지우고 state 상태값 초기화(아래)
     function logout() {
-        localStorage.clear();
         setUserInfo({
             user_name: '',
             user_email: '',
