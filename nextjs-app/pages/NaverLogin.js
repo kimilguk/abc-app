be5bf145921fbfef63d2946b5c26c901c2422a2e
kimilguk <boramcom@daum.net>
2023-01-13 23:24:38 +0900

    리액트+넥스트js 3일차 작업소스

--- nextjs-app/pages/NaverLogin.js
+++ nextjs-app/pages/NaverLogin.js
@@ -1,4 +1,5 @@
 //import React, { useEffect, useState, useRef } from 'react';
+import { SignpostOutlined } from "@mui/icons-material";
 import { useSession, signIn, signOut } from "next-auth/react"
 import Link from 'next/link';
 
@@ -11,12 +12,16 @@ function NaverLogin(props) {
             ?
                 <div>
                     Looged in as :<span id="user">{session.user.email} 님</span>
-                    <button onClick={() => signOut()} className="btn btn-info">로그아웃</button>
+                        <button onClick={() => signOut({callbackUrl: `/MovieListComponent`})} 
+                        className="btn btn-info">로그아웃</button>
                 </div>
             :
                 <>
-                <button onClick={() => signIn()} className="btn btn-success">네이버 로그인</button>
-                <span>&nbsp;</span><button onClick={() => signIn()} className="btn btn-info">일반로그인</button>
+                    <button onClick={() => signIn({callbackUrl: `/MovieListComponent`})}
+                    className="btn btn-success">네이버 로그인</button>
+                    <span>&nbsp;</span>
+                    <button onClick={() => signIn({callbackUrl: `/MovieListComponent`})}
+                    className="btn btn-info">일반로그인</button>
                 </>
             }
         </>
