31a1c08d4082ab5772487e28668e62e69c500a19
kimilguk <boramcom@daum.net>
2023-01-20 21:24:25 +0900

    리액트+넥스트js 14-3교시 작업소스

--- nextjs-app/pages/api/hello.ts
+++ nextjs-app/pages/api/hello.ts
@@ -9,8 +9,20 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  //res.status(200).json({ name: 'John Doe' })
+  console.log("자료: ", req.query['date']);
+  let searchDate: any = (req.query['date'])?req.query['date']:'20221225';
+  searchDate = searchDate.replace(/-/g,"");
+  const URL = 'http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt='+searchDate;
+        fetch(URL)
+            .then((response) => {
+                    return response.json();//프로미스 객체로 리턴 반환 된다.(아래 then 으로)
+                }
+            ).then(data => { //도트 연산자로 함수를 이어서 편리하게 사용하는 방식을 도트 체인함수라고 한다.
+                res.status(200).json(data)
+                console.log(data['boxOfficeResult']['dailyBoxOfficeList']);//크롬 브라우저에서 json데이터 확인
+            });
   //console.log(req.query.name);
   //let queryName: string = (req.query.name) as string;
   //res.status(200).json({ name: queryName })
-  res.status(200).json({ name: 'John Doe' })
 }
