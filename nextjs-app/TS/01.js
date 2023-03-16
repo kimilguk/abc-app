731867c27a85419cd7f7c9bd66db945995f2a997
kimilguk <boramcom@daum.net>
2023-03-16 17:19:51 +0900

    넥스트js환경에서 타입스크립트 기초

---
+++ nextjs-app/TS/01.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var user_id;
+user_id = "123";
+console.log(user_id);
+function getAdd(x, y) {
+    return x + y;
+}
+console.log(getAdd(10, 20));
---
+++ nextjs-app/TS/01.ts
@@ -0,0 +1,8 @@
+let user_id: string
+user_id = "123";
+console.log(user_id);
+function getAdd(x:number, y:number) {
+    return x + y;
+}
+console.log(getAdd(10, 20))
+export {}
